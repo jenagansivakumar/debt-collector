83a028434d0ed841869368f046d65c85a898f60c
Jena <jenagansivakumar@gmail.com>
2023-10-05 10:51:04 +0200

    creating logic

--- src/Components/DebtCalculator.tsx
+++ src/Components/DebtCalculator.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useState, FormEvent } from "react";
 
 export default function DebtCalculator() {
-  return <div>DebtCalculator</div>;
+  const [input, setInput] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const numbers = input.match(/[-+]?\d*\.\d+|\d+/g);
+    if(numbers)
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={input}
+          placeholder="Enter the Debt to Collect"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" onSubmit={handleSubmit}>
+          {" "}
+          Collect The Debt!
+        </button>
+      </form>
+    </div>
+  );
 }
